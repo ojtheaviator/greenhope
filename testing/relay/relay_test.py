170f5f4fd09e576e342bdc540f6941506607d62c
Owen Johnson <owenhljohnson@gmail.com>
2024-04-16 16:05:37 -0900

    it freakin works and added a new relay tester thing

--- testing/relay/relay_test.py
+++ testing/relay/relay_test.py
@@ -1,15 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-led = 29
+led = [29, 31, 33][int(input("0, 1, or 2?: "))]
+
+#light is 29
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led, GPIO.OUT)
 
-for i in range(4):
+'''
+for i in range(2):
     GPIO.output(led, GPIO.HIGH)
-    time.sleep(0.2)
+    time.sleep(10)
     GPIO.output(led, GPIO.LOW)
-    time.sleep(0.2)
+    time.sleep(10)
+'''
+
+GPIO.output(led, GPIO.HIGH)
+input("enter to continue")
+GPIO.output(led, GPIO.LOW)
 
 GPIO.cleanup()
