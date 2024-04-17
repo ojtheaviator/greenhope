b088f59bd1308c19124664d4150ce898cd385271
Owen Johnson <owenhljohnson@gmail.com>
2024-04-17 06:33:58 -0900

    more testing module

--- testing/relay/relay_test.py
+++ testing/relay/relay_test.py
@@ -1,13 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
-led = [29, 31, 33][int(input("0, 1, or 2?: "))]
+#led = [29, 31, 33][int(input("0, 1, or 2?: "))]
+#led = [29, 31][int(input("0 or 1?: "))]
 
 #light is 29
 
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led, GPIO.OUT)
+GPIO.setup(29, GPIO.OUT)
+GPIO.setup(31, GPIO.OUT)
 
 '''
 for i in range(2):
@@ -15,10 +17,18 @@ for i in range(2):
     time.sleep(10)
     GPIO.output(led, GPIO.LOW)
     time.sleep(10)
-'''
+
 
 GPIO.output(led, GPIO.HIGH)
 input("enter to continue")
 GPIO.output(led, GPIO.LOW)
+'''
+
+GPIO.output(29, GPIO.HIGH)
+GPIO.output(31, GPIO.HIGH)
+input("enter to continue")
+GPIO.output(29, GPIO.LOW)
+GPIO.output(31, GPIO.LOW)
+
 
 GPIO.cleanup()
