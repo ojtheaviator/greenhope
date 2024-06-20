360606638ffdfe443c9f07a09e13b36ae7a7c5d1
ojtheaviator <68511855+ojtheaviator@users.noreply.github.com>
2024-06-20 10:21:19 -0400

    Update install.sh
    Fixed a condition for the hub/module choice, moved hub-only install bits into the hub branch

--- install.sh
+++ install.sh
@@ -21,9 +21,13 @@ else
     exit
 fi
 
+echo "ensuring Pi is up to date"
+sudo apt update
+sudo apt upgrade
+
 runv1=0
 runv2=0
-while [[ $runv1 == 0 ]]; do
+while [[ $runv1 -eq 0 && $runv2 -eq 0 ]]; do
     read -p "Are you running as the Hub or a Module (h/m) " hm
     case $hm in
         [Hh]* ) runv1=1; break;;
@@ -35,26 +39,24 @@ done
 if [ $runv1 -eq 1 ]; then #running as the hub
 		echo "Installing dependencies for the hub"
 		sudo pip install -r hub/requirements.txt
+
+		echo "installing mariadb-server"
+		sudo apt install mariadb-server
+		echo "follow prompts for MySQL below (most secure install requires 'y' to all y/n prompts). Be sure to write down the password you create in this process: "
+		sudo mysql_secure_installation
+		
+		echo "installing Grafana"
+		sudo mkdir -p /etc/apt/keyrings/
+		wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+		echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+		sudo /bin/systemctl enable grafana-server
+		sudo /bin/systemctl start grafana-server
 else #running as the module
 		echo "Installing dependencies for the module"
 		sudo pip install -r module/requirements.txt
 fi
 
-echo "ensuring Pi is up to date"
-sudo apt update
-sudo apt upgrade
 
-echo "installing mariadb-server"
-sudo apt install mariadb-server
-echo "follow prompts for MySQL below (most secure install requires 'y' to all y/n prompts). Be sure to write down the password you create in this process: "
-sudo mysql_secure_installation
-
-echo "installing Grafana"
-sudo mkdir -p /etc/apt/keyrings/
-wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
-sudo /bin/systemctl enable grafana-server
-sudo /bin/systemctl start grafana-server
 
 echo 
 echo 
