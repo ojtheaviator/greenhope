9992c974af2f8786e8238fcc2d4afea74c7e4e77
ojtheaviator <68511855+ojtheaviator@users.noreply.github.com>
2024-06-21 11:21:21 -0400

    Update install.sh
    added stuff for can hat

--- install.sh
+++ install.sh
@@ -36,6 +36,39 @@ while [ $runv1 -eq 0 ] && [ $runv2 -eq 0 ]; do
     esac
 done
 
+echo "installing CAN hat dependencies"
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.71.tar.gz
+tar zxvf bcm2835-1.71.tar.gz 
+cd bcm2835-1.71/
+sudo ./configure && sudo make && sudo make check && sudo make install
+
+cd
+sudo apt-get install wiringpi
+#For Raspberry Pi systems after May 2019 (earlier than that can be executed without), an upgrade may be required:
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+gpio -v
+# Run gpio -v and version 2.52 will appear, if it doesn't it means there was an installation error
+
+# Bullseye branch system using the following command:
+git clone https://github.com/WiringPi/WiringPi
+cd WiringPi
+. /build
+gpio -v
+
+wget https://github.com/joan2937/lg/archive/master.zip
+unzip master.zip
+cd lg-master
+sudo make install
+#Raspberry Pi 5 
+sudo apt install python3-rpi-lgpio
+
+sudo apt-get install python3-serial
+sudo apt-get install python3-can
+
+sudo echo "dtparam=spi=on" >> /boot/config.txt
+sudo echo "dtoverlay=mcp2515-can0,oscillator=12000000,interrupt=25,spimaxfrequency=2000000" >> /boot/config.txt
+
 if [ $runv1 -eq 1 ]; then #running as the hub
 		echo "Installing dependencies for the hub"
 		sudo pip install -r hub/requirements.txt
